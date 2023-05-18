f1502bfe1a93bc1822e87968bf6bc5dedf1d0f22
Sami Kalammallah <skalexsong@gmail.com>
2023-05-18 23:23:13 +0700

    feat: create basic api

---
+++ src/pages/api/lives.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json([
+    { img: "https://links.papareact.com/2io", title: "Outdoor getaways" },
+    { img: "https://links.papareact.com/q7j", title: "Unique stays" },
+    { img: "https://links.papareact.com/s03", title: "Entire homes" },
+    { img: "https://links.papareact.com/8ix", title: "Pet allowed" },
+  ]);
+}
