bfb9deef5b81b480882163018bf725e5bf5d9249
Sami Kalammallah <skalexsong@gmail.com>
2023-05-18 23:23:24 +0700

    feat: finish basic landing page

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { AIRBNB_LOGO } from "@/constant/URLS";
+import Image from "next/image";
+import {
+  Bars3Icon,
+  GlobeAltIcon,
+  MagnifyingGlassIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/solid";
+
+const Header = () => (
+  <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 items-center">
+    <div className="flex relative items-center h-10 cursor-pointer">
+      <Image
+        src={AIRBNB_LOGO}
+        alt="Airbnb Logo"
+        fill
+        style={{ objectFit: "contain" }}
+      />
+    </div>
+    <div className="flex items-center justify-between py-1 md:border-2 md:shadow-sm rounded-full">
+      <input
+        type="text"
+        name="search"
+        placeholder="Start your search | Where ever you go"
+        className="flex-grow pl-5 outline-none bg-transparent text-sm text-gray-600 placeholder-gray-400"
+      />
+      <MagnifyingGlassIcon className="hidden md:inline-flex md:mx-1 h-8 bg-red-500 text-white rounded-full p-2" />
+    </div>
+    <div className="flex space-x-4 justify-end items-center text-gray-500">
+      <p className="hidden md:inline cursor-pointer">Become a host</p>
+      <GlobeAltIcon className="h-6 cursor-pointer" />
+
+      <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+        <Bars3Icon className="h-6 cursor-pointer" />
+        <UserCircleIcon className="h-6 cursor-pointer" />
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
---
+++ src/constant/URLS.ts
@@ -0,0 +1,2 @@
+export const AIRBNB_LOGO = "https://links.papareact.com/qd3";
+export const AIRBNB_BANNER = "https://links.papareact.com/0fm";
