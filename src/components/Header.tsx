0244ac09cc51ff3f839b3667947605f52ffebe32
Sami Kalammallah <skalexsong@gmail.com>
2023-06-01 17:31:58 +0700

    feat: add guest input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,14 @@ import {
   GlobeAltIcon,
   MagnifyingGlassIcon,
   UserCircleIcon,
+  UsersIcon,
 } from "@heroicons/react/24/solid";
 
 const Header = () => {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
 
   const selectionRange = {
     startDate,
@@ -29,6 +31,8 @@ const Header = () => {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate);
   };
+
+  const handleInputGuest = (e) => setNoOfGuests(e.target.value);
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 items-center">
@@ -69,6 +73,21 @@ const Header = () => {
             rangeColors={["#FD5B61"]}
             onChange={handleRangePicker}
           />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl font-semibold flex-grow">
+              Number of Guests
+            </h2>
+
+            <UsersIcon className="h-5" />
+
+            <input
+              type="number"
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+              min={1}
+              value={noOfGuests}
+              onChange={handleInputGuest}
+            />
+          </div>
         </div>
       )}
     </header>
